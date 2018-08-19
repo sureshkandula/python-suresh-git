b28c97a8a04ebb9b33d0993557d71faca5a62f45
sureshkandula <sureshbabu110@gmail.com>
2018-08-19 12:40:12 -0700

    my first file on the name of saibaba

---
+++ saibaba.py
@@ -0,0 +1,12 @@
+print('hello world ..its my world')
+a='suresh'
+b='siri'
+c='shrutika is a good girl'
+d='r'
+print('here is my first program using pycham:-', a,b,c)
+print('i am passing variables from the line itslef  {}' .format('see its displayed'))
+print('type,lenghth and id of the variable:-' , len(a),type(b),id(c))
+print('replacing the letter in a variable:-', c.replace('shrutika','shrutika'))
+print('finding slicing or count:', c[4])
+print ('slicing :- ', c[4:])
+print('finding index and rindex:-', c.index(d, 0,5))
